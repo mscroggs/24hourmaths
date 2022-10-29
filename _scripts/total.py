518ea02a0718b73966ab302c55443e9398ef63d0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-10-29 14:22:37 +0100

    Pi thousand

--- _scripts/total.py
+++ _scripts/total.py
@@ -55,5 +55,9 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../totalise
     f.write("layout: totaliser\n")
     f.write("---\n")
 
-    f.write("<div style='text-align:center;font-size:300%;padding-top:30px'>Money raised so far:</div>\n"
-            f"<div style='font-weight:bold;text-align:center;font-size:400%'>{money}</div>\n")
+    if pence >= 314159 and pence < 350000:
+        f.write("<div style='text-align:center;font-size:300%;padding-top:30px'>Money raised so far:</div>\n"
+                f"<div style='font-weight:bold;text-align:center;font-size:400%'>£1000&pi;</div>\n")
+    else:
+        f.write("<div style='text-align:center;font-size:300%;padding-top:30px'>Money raised so far:</div>\n"
+                f"<div style='font-weight:bold;text-align:center;font-size:400%'>{money}</div>\n")
